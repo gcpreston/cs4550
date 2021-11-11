cd2619e5a0cc41d92ce36ec0298fe4048905bc38
Graham Preston <graham.preston@gmail.com>
2021-11-10 20:32:49 -0500

    Input

--- src/components/a7/Build/WhatsHappening/index.js
+++ src/components/a7/Build/WhatsHappening/index.js
@@ -14,24 +14,39 @@ const WhatsHappening = () => {
 
   return (
     <>
-      <img className="rounded-circle wd-avatar-image"
-        src='/images/react.png'
-        alt='icon' />
-      <textarea className="form-control"
-        value={whatsHappening}
-        placeholder="What's happening?"
-        onChange={(event) =>
-          setWhatsHappening(event.target.value)}>
-      </textarea>
-
-      <i className="fas fa-image"></i>
-      <i className="fas fa-chart-bar"></i>
-      <i className="fas fa-face-smile"></i>
-      <i className="fas fa-calendar"></i>
-
-      <button className="btn btn-primary rounded-pill" onClick={tweetClickHandler}>
-        Tweet
-      </button>
+      <table style={{ marginBottom: '16px' }}>
+        <tr>
+          <td style={{ verticalAlign: 'top' }}>
+            <img src={'/images/react.png'}
+              className="rounded-circle"
+              style={{ width: '48px', margin: '16px' }}
+              alt='profile-icon'
+            />
+          </td>
+          <td style={{ width: "100%" }}>
+            <textarea value={whatsHappening}
+              onChange={(event) => setWhatsHappening(event.target.value)}
+              className="form-control"
+              style={{
+                width: "100%", color: "white",
+                padding: "0px",
+                paddingTop: "15px",
+                backgroundColor: "black"
+              }}
+              placeholder="What's happening?"></textarea>
+            <hr />
+            <span>
+              <a href="/#"><i className="far fa-image me-3"></i></a>
+              <a href="/#"><i className="fas fa-chart-line me-3"></i></a>
+              <a href="/#"><i className="far fa-smile me-3"></i></a>
+              <a href="/#"><i className="far fa-calendar me-3"></i></a>
+            </span>
+            <button onClick={tweetClickHandler} className="btn btn-primary fa-pull-right rounded-pill">
+              Tweet
+            </button>
+          </td>
+        </tr>
+      </table>
     </>
   );
 }
